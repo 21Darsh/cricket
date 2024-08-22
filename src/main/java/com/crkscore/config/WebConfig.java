3050a340ebbc922d8fb8b3eb9288e4d89f4aa8f0
21Darsh <darsh.pardeshi@gmail.com>
2024-08-22 21:21:54 +0530

    Update WebConfig.java

--- src/main/java/com/crkscore/config/WebConfig.java
+++ src/main/java/com/crkscore/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://crick-score-fe.vercel.app/") // Replace with your Vercel app URL
+                .allowedOrigins("https://cricket-ashy.vercel.app/") // Replace with your Vercel app URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
